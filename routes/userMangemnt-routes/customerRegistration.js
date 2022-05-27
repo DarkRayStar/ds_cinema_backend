ed5b1f58610fe9e579ff83b9439e98ad2c4d5bab
kushaniSakalasooriya <79516521+kushaniSakalasooriya@users.noreply.github.com>
2022-05-27 16:46:11 +0530

    update messages

--- routes/userMangemnt-routes/customerRegistration.js
+++ routes/userMangemnt-routes/customerRegistration.js
@@ -10,25 +10,19 @@ router.post("/", async (req, res) => {
 			return res.json('invalid password! (Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters)');
 		}
 			
-
+		//check requested body email
 		const user = await User.findOne({ email: req.body.email });
 		if (user){
-			// return res
-			// 	.status(409)
-			// 	.send({ message: "User with given email already Exist!" });
 			return res.json('User with given email already Exist!');
           
 		}
 			
-
+		//converted into hash password using bcrypt dependency
 		const salt = await bcrypt.genSalt(Number(process.env.SALT));
 		const hashPassword = await bcrypt.hash(req.body.password, salt);
 
-		// await new User({ ...req.body, password: hashPassword }).save();
-		// res.status(201).send({ message: "User created successfully" });
-
 		await new User({ ...req.body, password: hashPassword }).save()
-		.then(() => res.json('User created successfully!'))
+		.then(() => res.json('User created successfully! Please login to the system'))
             .catch(err => res.status(400).json('Error: ' + err));
 
 	} catch (error) {
